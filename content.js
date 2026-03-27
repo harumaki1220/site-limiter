3f52a434af07c9e13acae6d08fc086bf4d68826a
harumaki1220 <haruma181220@gmail.com>
2026-03-27 23:35:48 +0900

    feat: X(Twitter)を封印する基本機能を実装

---
+++ content.js
@@ -0,0 +1,15 @@
+document.body.innerHTML = `
+  <div style="
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #000;
+    color: #fff;
+    font-family: sans-serif;
+    text-align: center;
+  ">
+    <h1 style="font-size: 4rem; margin-bottom: 20px;">命を削ってコードを書け</h1>
+  </div>
+`;
